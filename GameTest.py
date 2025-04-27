19328942990ab6fb22494f29812ae5d38f49a43b
AlwaysWorkingNeverOver <ppickett76@gmail.com>
2025-04-27 14:54:17 -0400

    Menu thread update

--- GameTest.py
+++ GameTest.py
@@ -1,103 +1,114 @@
-import threading
-import pygame
-import socket
-import sys
+"""
+Importing important libraries
+"""
+import pygame, sys
 
-name = "test"
-posx = 300
-posy = 200
-speed = 10
-
-def GameThread():
-    pygame.init()
-    background = (204, 230, 255)
-    shapeColor = (0, 51, 204)
-    shapeColorOver = (255, 0, 204)
-    dropColor = (255, 0, 0)
-    
-    fps = pygame.time.Clock()
-    screen_size = screen_width, screen_height = 600, 400
-    rect2 = pygame.Rect(0, 0, 600, 400)
-    rect1 = pygame.Rect(0, 0, 30, 30)
-    rect3 = pygame.Rect(0,0, 20, 20)
-    screen = pygame.display.set_mode(screen_size)
-    pygame.display.set_caption('Test')
-    
-    colorRect = (shapeColor)
-    colorRect2 = (shapeColorOver)
-    colorRect3 = (dropColor)
-    global posx 
-    global posy 
+"""
+Setting up an environment to initialize pygame
+"""
+mainClock = pygame.time.Clock()
+from pygame.locals import *
+pygame.init()
+pygame.display.set_caption('game base')
+screen = pygame.display.set_mode((600, 300),0,32)
+ 
+#setting font settings
+font = pygame.font.SysFont(None, 30)
+ 
+"""
+A function that can be used to write text on our screen and buttons
+"""
+def draw_text(text, font, color, surface, x, y):
+    textobj = font.render(text, 1, color)
+    textrect = textobj.get_rect()
+    textrect.topleft = (x, y)
+    surface.blit(textobj, textrect)
+ 
+# A variable to check for the status later
+click = False
+ 
+# Main container function that holds the buttons and game functions
+def main_menu():
     while True:
+ 
+        screen.fill((0,190,255))
+        draw_text('Main Menu', font, (0,0,0), screen, 250, 40)
+ 
+        mx, my = pygame.mouse.get_pos()
+
+        #creating buttons
+        button_1 = pygame.Rect(200, 100, 200, 50)
+        button_2 = pygame.Rect(200, 180, 200, 50)
+
+        #defining functions when a certain button is pressed
+        if button_1.collidepoint((mx, my)):
+            if click:
+                game()
+        if button_2.collidepoint((mx, my)):
+            if click:
+                options()
+        pygame.draw.rect(screen, (255, 0, 0), button_1)
+        pygame.draw.rect(screen, (255, 0, 0), button_2)
+ 
+        #writing text on top of button
+        draw_text('PLAY', font, (255,255,255), screen, 270, 115)
+        draw_text('OPTIONS', font, (255,255,255), screen, 250, 195)
+
+
+        click = False
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+            if event.type == QUIT:
                 pygame.quit()
                 sys.exit()
-
-        screen.fill(background)
-        rect1.center = (posx, posy)
-        collision = rect1.colliderect(rect2)
-
-        pygame.draw.rect(screen, colorRect3, rect3, 6, 1)
-        pygame.draw.rect(screen, colorRect, rect1)
-        if collision:
-            pygame.draw.rect(screen, colorRect2, rect2, 6, 1)
-        else:
-            pygame.draw.rect(screen, colorRect, rect2, 6, 1)
-        
-        pygame.draw.rect(screen, colorRect3, rect3, 6, 1)
-
-        #dropped rect dances down the page
-        rect3.move_ip(0, speed * fps.tick(60))
-
-
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    pygame.quit()
+                    sys.exit()
+            if event.type == MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    click = True
+ 
         pygame.display.update()
-        fps.tick(60)
+        mainClock.tick(60)
+ 
+"""
+This function is called when the "PLAY" button is clicked.
+"""
+def game():
+    running = True
+    while running:
+        screen.fill((0,0,0))
+       
+        draw_text('GAME SCREEN', font, (255, 255, 255), screen, 20, 20)
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    running = False
+       
+        pygame.display.update()
+        mainClock.tick(60)
 
-
-    pygame.quit()
-
-
-def ServerThread():
-    global posy
-    global posx
-    # get the hostname
-    host = socket.gethostbyname(socket.gethostname())
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.connect(("localhost", 80))
-    host = s.getsockname()[0]
-    s.close()
-    print(host)
-    port = 5000  # initiate port no above 1024
-
-    server_socket = socket.socket()  # get instance
-    # look closely. The bind() function takes tuple as argument
-    server_socket.bind((host, port))  # bind host address and port together
-    print("Server enabled...")
-    # configure how many client the server can listen simultaneously
-    server_socket.listen(2)
-    conn, address = server_socket.accept()  # accept new connection
-    print("Connection from: " + str(address))    
-    while True:        
-        # receive data stream. it won't accept data packet greater than 1024 bytes
-        data = conn.recv(1024).decode()
-        if not data:
-            # if data is not received break
-            break
-        
-        print("from connected user: " + str(data))
-        if(data == 'w'):
-            posy -= 10
-        if(data == 's'):
-            posy += 10
-        if(data == 'a'):
-            posx -= 10
-        if(data == 'd'):
-            posx += 10
-    conn.close()  # close the connection
-
-
-t1 = threading.Thread(target=GameThread, args=[])
-t2 = threading.Thread(target=ServerThread, args=[])
-t1.start()
-t2.start()
+"""
+This function is called when the "OPTIONS" button is clicked.
+"""
+def options():
+    running = True
+    while running:
+        screen.fill((0,0,0))
+ 
+        draw_text('OPTIONS SCREEN', font, (255, 255, 255), screen, 20, 20)
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    running = False
+       
+        pygame.display.update()
+        mainClock.tick(60)
+ 
+main_menu()
